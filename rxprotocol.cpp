248e5d494a3814dcce9bb6bcc4515f71d910fa27
iassasin <iassasin@yandex.ru>
2017-02-22 16:13:36 +0300

    removed File parameter, added FILE command

--- rxprotocol.cpp
+++ rxprotocol.cpp
@@ -39,51 +39,59 @@ void RXProtocol::process(istream &in, ostream &out){
 			continue;
 		
 		vector<string> args = regex_split(line, regex("\\s+"), 2);
+		unordered_map<string, string> params;
 		
+		//command and argument (CMD arg)
 		string cmd = args[0], arg = args.size() > 1 ? args[1] : "";
 		
+		//read parameters loop
+		while (getline(in, line)){
+			if (line.empty())
+				break;
+
+			vector<string> pars = regex_split(line, regex(":\\s*"), 2);
+			if (pars.size() < 2){
+				Log::warn("Non-parameter line, skipping: ", line);
+				continue;
+			}
+
+			Log::debug("Parameter: ", pars[0], ": ", pars[1]);
+			params[pars[0]] = pars[1];
+		}
+
 		if (cmd == "EXIT"){
 			Log::debug("Client exit by protocol");
 			return;
 		}
+		else if (cmd == "FILE"){
+			auto pname = params.find("Name");
+			auto psize = params.find("Size");
+			if (pname != params.end() && psize != params.end()){
+				string tmpath = workspace.getWorkdir();
+				string name = regex_replace(pname->second, regex("\\.{1,2}/"), "");
+				size_t size = stoull(psize->second);
+
+				Log::debug("Appended file: ", name, " ", size);
+
+				ofstream of(tmpath + name, ios::binary);
+				bscopy(in, of, size);
+				of.close();
+
+				in.get(); in.get(); // '\n\n'
+
+				out << "OK" << endl;
+			} else {
+				out << "ERROR 3 Name or Size parameter not set" << endl;
+			}
+		}
 		else if (cmd == "EXEC"){
-			unordered_map<string, string> params;
 			string task = realpath(Config::getString(Config::TASK_DIR)) + "/" + arg;
 			string tasktmp = workspace.getWorkdir();
 			
 			Log::debug("Task binary: ", task);
 			Log::debug("Task tmp: ", tasktmp);
-			
-			bool valid = path_exists(task);
-			
-			while (getline(in, line)){
-				if (line.empty())
-					break;
-				
-				vector<string> pars = regex_split(line, regex(":\\s*"), 2);
-				if (pars.size() < 2){
-					Log::warn("Non-parameter line, skipping: ", line);
-					continue;
-				}
-				if (pars[0] == "File"){
-					Log::debug("Appended file: ", pars[1]);
-					
-					vector<string> filev = regex_split(pars[1], regex("\\s+", 2));
-					size_t fsz = stoll(filev[0]);
-					string fn = regex_replace(filev[1], regex("\\.{1,2}/"), "");
-					
-					ofstream of(tasktmp + fn, ios::binary);
-					bscopy(in, of, fsz);
-					of.close();
-					
-					in.get(); //get '\n'
-				} else {
-					Log::debug("Parameter: ", pars[0], ": ", pars[1]);
-					params[pars[0]] = pars[1];
-				}
-			}
-			
-			if (valid){
+
+			if (path_exists(task)){
 				Task t(arg, task, tasktmp);
 				out << "OK" << endl;
 				t.run(out);
